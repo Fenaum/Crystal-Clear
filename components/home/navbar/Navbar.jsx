66aadcfb27f88971a248f6f60ada568cc9d4f3c9
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-04-03 14:59:38 -0700

    added layout

---
+++ components/home/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {icons} from "../../../constants"
+
+const Navbar = () => {
+  return (
+    <View>
+    </View>
+  )
+}
+
+export default Navbar
